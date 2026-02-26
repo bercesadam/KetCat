268a5802871455e28772c4c14e5c9215398c6721
bercesadam <adam.berces@googlemail.com>
2026-02-26 19:59:35 +0000

    Add Reduced Energy Hilbert space representation

---
+++ include/hilbert_space/reduced_energy_space.h
@@ -0,0 +1,148 @@
+#pragma once
+#include "hilbert_space/state_vector.h"
+#include "wavefunction/wavefunction_concept.h"
+
+namespace KetCat
+{
+
+    /// @brief Reduced Hilbert space constructed from an arbitrary set of wavefunction seeds.
+    ///
+    /// @tparam GridDim    Number of discretization steps per spatial axis.
+    /// @tparam Extent     Spatial extent of the grid.
+    /// @tparam LevelCount Number of retained basis states in the reduced space.
+    ///
+    /// ------------------------------------------------------------------------
+    /// Mathematical construction
+    /// ------------------------------------------------------------------------
+    ///
+    /// Full spatial Hilbert space:
+    ///
+    ///      𝓗_grid  ≅  ℂ^(GridDim²)
+    ///
+    /// Seed wavefunctions:
+    ///
+    ///      ψ_i(x,y)  (provided by a user-defined generator)
+    ///
+    /// We select LevelCount states:
+    ///
+    ///      { |φ₀⟩, |φ₁⟩, ..., |φ_{K-1}⟩ }
+    ///
+    /// and define the reduced subspace:
+    ///
+    ///      𝓗_red = span{ |φ_i⟩ }
+    ///
+    /// which is isomorphic to ℂ^K.
+    ///
+    /// This provides a physically meaningful qudit or truncated subspace
+    /// suitable for simulation.
+    ///
+    /// ------------------------------------------------------------------------
+    /// Physical meaning
+    /// ------------------------------------------------------------------------
+    ///
+    /// Each basis vector corresponds to a physical mode of the system
+    /// as defined by the generator.
+    ///
+    /// Logical states can later be defined as selected combinations
+    /// of these physical basis states.
+    ///
+    /// This object bridges:
+    ///
+    ///      InfiniteHilbertSpace ↔ FiniteHilbertSpace
+    ///
+    /// allowing full state-vector simulation in a reduced, physically
+    /// meaningful subspace.
+    template<natural_t GridDim, real_t Extent, natural_t LevelCount>
+    struct ReducedEnergySpace
+    {
+        using GridHilbertSpace = InfiniteHilbertSpace2D<GridDim, Extent>;
+        using ReducedHilbertSpace = FiniteHilbertSpace<LevelCount>;
+
+        /// Embedded basis states |φ_i⟩ represented in the full grid space
+        std::array<StateVector<GridHilbertSpace>, LevelCount> Basis;
+
+        /// @brief Construct the reduced space from an arbitrary wavefunction generator.
+        ///
+        /// The generator must satisfy the `wavefunction_generator_t` concept.
+        /// Each parameter tuple provides inputs to generate one basis state.
+        ///
+        /// @param generator Callable that produces a wavefunction from a parameter tuple
+        /// @param params    Array of LevelCount parameter tuples for the generator
+        template<typename Generator, typename ParamTuple>
+            requires wavefunction_generator_t<Generator, ParamTuple>
+        ReducedEnergySpace(Generator&& generator, const std::array<ParamTuple, LevelCount>& params)
+        {
+            for (natural_t i = 0; i < LevelCount; ++i)
+            {
+               Basis[i] = std::apply(generator, params[i]);
+            }
+
+            orthonormalize();
+        }
+
+        /// @brief Orthonormalize the basis using Gram–Schmidt.
+        ///
+        /// Ensures the basis satisfies:
+        ///
+        ///      ⟨φ_i | φ_j⟩ = δ_ij
+        constexpr void orthonormalize() noexcept
+        {
+            for (natural_t i = 0; i < LevelCount; ++i)
+            {
+                for (natural_t j = 0; j < i; ++j)
+                {
+                    auto Proj = Basis[j].innerProduct(Basis[i]);
+                    for (natural_t k = 0; k < GridHilbertSpace::Dim; ++k)
+                    {
+                        Basis[i][k] = Basis[i][k] - Proj * Basis[j][k];
+                    }
+                }
+                Basis[i].normalize();
+            }
+        }
+
+        /// @brief Project a full spatial state into the reduced subspace.
+        ///
+        /// Computes coefficients:
+        ///
+        ///      c_i = ⟨φ_i | ψ⟩
+        ///
+        /// Resulting reduced state:
+        ///
+        ///      |ψ⟩ → (c₀, ..., c_{K-1})
+        constexpr StateVector<ReducedHilbertSpace>
+            project(const StateVector<GridHilbertSpace>& psi) const noexcept
+        {
+            StateVector<ReducedHilbertSpace> Coeffs{};
+
+            for (natural_t i = 0; i < LevelCount; ++i)
+            {
+                Coeffs[i] = Basis[i].innerProduct(psi);
+            }
+
+            return Coeffs;
+        }
+
+        /// @brief Embed a reduced state back into the full grid space.
+        ///
+        /// Computes:
+        ///
+        ///      |ψ_red⟩ = Σ_i c_i |φ_i⟩
+        constexpr StateVector<GridHilbertSpace>
+            embed(const StateVector<ReducedHilbertSpace>& coeffs) const noexcept
+        {
+            StateVector<GridHilbertSpace> Psi{ cplx_t::zero() };
+
+            for (natural_t i = 0; i < LevelCount; ++i)
+            {
+                for (natural_t k = 0; k < GridHilbertSpace::Dim; ++k)
+                {
+                    Psi[k] += coeffs[i] * Basis[i][k];
+                }
+            }
+
+            return Psi;
+        }
+    };
+
+}
