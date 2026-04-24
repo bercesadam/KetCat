2e47f2c3dbd58e5ecf26918dbd9bb52fd6a8675f
bercesadam <adam.berces@googlemail.com>
2026-04-24 13:45:16 +0000

    Fix typo on filename; Add laser protocol stub

---
+++ include/laser/laser_drive_protocol.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "neutral_atom_manifold.h"
+
+
+namespace KetCat
+{
+    struct LaserPulse
+    {
+        real_t m_waveLengthNm;
+        real_t m_intensityWCm2;
+    };
+
+    class LaserDriveProtocol
+    {
+
+    };
+
+}
