26616280f3cdce87b8dfd0a58b5853810fb30c98
Berces <Berces@DESKTOP-P67N6NB>
2026-04-29 15:24:30 +0200

    Add smoke test (Pauli-X gate) for a Cesium-based qubit

--- src/qdit_test.cpp
+++ src/qdit_test.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-#include <iomanip>
+
 #include <memory>
+#include <functional>
 
 #include "systems/neutral_atom_manifold.h"
-#include "laser/laser_drive_protocol.h"
+#include "laser/single_qbit_protocol.h"
+#include "kwf_exporter/simulation_view_builder.h"
 #include "kwf_exporter/kwf_exporter.h"
 
 
@@ -17,56 +18,50 @@ int main()
     NeutralAtomTypeConfig
     <
         Element::Cs, 256, 75.0,
-        0, 1, 4,
+        0, 2, 4,
         QuantumNumber<6, s>,
         QuantumNumber<6, p>,
         QuantumNumber<7, s>,
         QuantumNumber<7, p>,
-        QuantumNumber<40, s>,
+        QuantumNumber<40, s>/*,
         QuantumNumber<40, p>,
         QuantumNumber<39, s>,
         QuantumNumber<39, p>,
         QuantumNumber<5, d>,
-        QuantumNumber<6, d>
+        QuantumNumber<100, f>*/
     > Config;
 
 	NeutralAtomManifold<Config> Manifold;
     using HilbertSpace = typename decltype(Manifold)::SingleAtomFullHilbertSpace;
+	using OperationHilbertSpace = typename decltype(Manifold)::SingleAtomOperationHilbertSpace;
 
+    SimulationViewBuilder<Config> ViewBuilder(Manifold);
     StateVectorExporter<HilbertSpace> Exporter
     (
         "simulation.kwf",
         KetCat::ExportMode::RealImag
     );
 
-    auto ExporterCallback = [&](real_t time, const auto& currentPsi)
-    {
-
-        std::ostringstream Title;
-        Title << std::fixed << std::setprecision(2);
-        Title << "Cesium (Z=55)|";
-        //Title << "Laser: " << wavelength_nm << " nm; " << requiredIntensity_Wcm2 << " W/cm²|";
-        Title << "Time: " << time << " a.u.|";
-        Title << "Populations: ";
-        Title << "6s: " << currentPsi[0].normSquared() * 100.0 << "% ";
-        Title << "6p: " << currentPsi[1].normSquared() * 100.0 << "% ";
-        Title << "7s: " << currentPsi[2].normSquared() * 100.0 << "% ";
-        Title << "7p: " << currentPsi[3].normSquared() * 100.0 << "% ";
-
-        auto Psi2D = Manifold.projectToFullHilbertSpace(currentPsi);
-        Exporter.writeTimestep(time, Psi2D, Title.str(), currentPsi[0], currentPsi[2]);
-
-        for (natural_t i = 0; i < Config.LevelCount; ++i)
+    natural_t FrameCounter = 0;
+    std::function<void(real_t, const StateVector<OperationHilbertSpace>&, const LaserPulse&, const LaserPulse&)> ExporterCallback =
+        [&](real_t time, const StateVector<OperationHilbertSpace>& currentPsi, const LaserPulse& laser1, const LaserPulse& laser2)
         {
-            std::cout << "Probability of basis state" << i << ": " << currentPsi[i].normSquared() * 100.0 << "%" << std::endl;
+            if (FrameCounter % 500000 == 0)
+            {
+                auto SimulationView = ViewBuilder.build("Gate: Pauli-X", time, currentPsi, laser1, laser2);
+                Exporter.writeTimestep(SimulationView);
 
-        }
-        std::cout << "------------------------" << std::endl;
-    };
+                for (natural_t i = 0; i < Config.LevelCount; ++i)
+                {
+                    std::cout << "Probability of basis state" << i << ": " << currentPsi[i].normSquared() * 100.0 << "%" << std::endl;
+                }
+                std::cout << "------------------------" << std::endl;
+            }
 
-    auto Psi = Manifold.getOperationSeed();
-    LaserDrivePulse<Config> STIRAP;
-    STIRAP(Psi, Config.Logical0Level, { RotationAxis::X, ConstexprMath::Pi / 2 }, ExporterCallback);
-    STIRAP(Psi, Config.Logical0Level, { RotationAxis::Z, ConstexprMath::Pi / 2 }, ExporterCallback);
-    STIRAP(Psi, Config.Logical0Level, { RotationAxis::Y, ConstexprMath::Pi / 2 }, ExporterCallback);
+            FrameCounter++;
+        };
+
+    StateVector<OperationHilbertSpace> Psi = Manifold.getOperationSeed();
+    SingleQubitProtocol<Config> Protocol;
+	Protocol.applyPulseCommand({ RotationAxis::X, ConstexprMath::Pi }, Psi, ExporterCallback);
 }
